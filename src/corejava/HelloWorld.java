8f4b3a22a9f6b2eb9c8c196c9fdf4fd3048e59e9
N650DU <elan_zh@163.com>
2017-12-04 10:03:28 +0800

    改名

---
+++ src/corejava/HelloWorld.java
@@ -0,0 +1,11 @@
+package corejava;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+        for (int i = 0; i <100; i++) {
+            System.out.println("Hello World!!!");
+        }
+	}
+
+}
